9cc169dc6b0a626d3f48bea6feb1d74dd76dd59b
Stormfall13 <85984394+Stormfall13@users.noreply.github.com>
2022-09-29 07:39:45 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,16 +24,12 @@ for (let i = 0; i < open_modal.length; i++) {
     open_modal[i].onclick = function() { // клик на открытие
         modal.classList.add('modal_vis'); // добавляем видимость окна
         modal.classList.remove('bounceOutDown'); // удаляем эффект закрытия
-        body.classList.add('body_block'); // убираем прокрутку
-        body.style.overflow = "auto";
-        body.style.margin = "0px";
     };
 }
 close_modal.onclick = function() { // клик на закрытие
     modal.classList.add('bounceOutDown'); // добавляем эффект закрытия
     window.setTimeout(function() { // удаляем окно через полсекунды (чтобы увидеть эффект закрытия).
         modal.classList.remove('modal_vis'); 
-        body.classList.remove('body_block'); // возвращаем прокрутку
     }, 500);
 };
 
